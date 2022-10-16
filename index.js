7ffbe92b9ea5c912c412f5b9e11ae90205fb2098
Hytalia <38212538+SantiagoMQv@users.noreply.github.com>
2022-10-16 21:38:48 +0200

    Separación de responsabilidades

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const { faker } = require('@faker-js/faker')
+const routerApi = require('./routes');
+
 const app = express();
 const port = 3000;
 
@@ -7,45 +8,12 @@ app.get('/', (req, res) => {
   res.send('Hola mi server en express');
 });
 
-//** USERS **//
-app.get('/users', (req, res) => {
-  const {limit, offset} = req.query;
-  if(limit && offset){
-    res.json({
-      limit,
-      offset
-    });
-  }else{
-    res.send('No hay parámetros');
-  }
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Hola, soy una nueva ruta');
 });
 
-//** PRODUCTS **//
-app.get('/products', (req, res) => {
-  const products = []
-  const { size } = req.query;
-  const limit = size || 10;
-  // Genero 100 productos inventados gracias a la libreria faker
-  for (let index = 0; index < limit; index++) {
-    products.push({
-      name: faker.commerce.product(),
-      price: parseInt(faker.commerce.price(), 10),
-      image: faker.image.imageUrl()
-    });
-
-  }
-  res.json(products);
-})
-
-//** CATEGORIES **//
-app.get('/categories/:categoryID/products/:productId', (req, res) => {
-  const { categoryID, productId } = req.params;
-  res.json([{
-    categoryID,
-    productId
-  }]);
-})
+routerApi(app);
 
 app.listen(port, () => {
-  console.log("Mi port " + port);
+  console.log('Mi port' +  port);
 });
