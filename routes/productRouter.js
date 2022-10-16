dd71fdbdfdbad6b8998f50619530e37e17f17133
Hytalia <38212538+SantiagoMQv@users.noreply.github.com>
2022-10-17 00:32:53 +0200

    Introducción a servicios

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,19 +1,13 @@
 const express = require('express');
-const { faker } = require('@faker-js/faker')
+
+const ProductsService = require('./../services/ProductService');
 
 const router = express.Router();
+const service = new ProductsService();
 
 router.get('/', (req, res) => {
-  const products = [];
-  const { size } = req.query;
-  const limit = size || 10;
-  for (let index = 0; index < limit; index++) {
-    products.push({
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(), 10),
-      image: faker.image.imageUrl(),
-    });
-  }
+  const products = service.find();
+
   res.json(products);
 });
 
@@ -23,10 +17,9 @@ router.get('/filter', (req, res) => {
 
 router.get('/:id', (req, res) => {
   const { id } = req.params;
+  const product = service.findOne(id);
   res.json({
-    id,
-    name: 'Product 2',
-    price: 2000
+    product
   });
 });
 
