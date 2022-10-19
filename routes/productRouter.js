6cfe257434b3363c3574c581e8031d250cb245e6
Hytalia <38212538+SantiagoMQv@users.noreply.github.com>
2022-10-19 20:37:54 +0200

    corrección

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const ProductsService = require('./../services/product.service');
+const ProductsService = require('./../services/productService');
 
 const router = express.Router();
 const service = new ProductsService();
